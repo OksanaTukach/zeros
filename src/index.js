29ee712b591c78e863a016478f420ac86daa9d11
oksanatukach <oshabovta@mail.ru>
2019-10-19 23:34:51 +0300

    init: start task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function zeros(expression) {
-  // your solution
+  let arr = expression.split('*')
+  let countFives = 0;
+  let countTwos = 0;
+
+arr.forEach(element => {
+  let n = element.match(/!/g).length;
+  element = element.match(/\d+/);
+
+  for (element; element > 0; element -= n) {
+    let elem = element;
+    while (elem % 5 === 0) {
+      countFives++;
+      elem = elem/5;
+    }
+    while (elem %2 === 0){
+      countTwos++;
+      elem = elem/2;
+    }
+  }
+});
+if (countFives>countTwos) {
+  return countTwos;
+} else {
+  return countFives;
+}
 }
